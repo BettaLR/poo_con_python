a2fb1797c088bc385d2b940bd4c0ecac03bee481
BettaLR <alexcanolara818@gmail.com>
2025-01-10 08:55:46 -0600

    Creando el método atacar

--- poo_con_python.py
+++ poo_con_python.py
@@ -2,14 +2,51 @@ class Personaje:
     #Atributos de la clase
     nombre = 'default'
     fuerza = 0
-    Inteligencia = 0 
-    Defensa = 0
+    inteligencia = 0 
+    defensa = 0
     vida = 0
+    
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        self.nombre = nombre     #El signo de igual es una asignasion
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+    
+    def atributos(self):
+        print(self.nombre)
+        print("-Fuerza: ", self.fuerza)
+        print("-Inteligencia: ", self.inteligencia)
+        print("-Defensa: ", self.defensa)
+        print("-Vida: ", self.vida)
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza += fuerza
+        self.inteligencia += inteligencia
+        self.defensa += defensa
+    
+    def esta_vivo(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+    
+    def dañar(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        daño = self.dañar(enemigo)
+        enemigo.vida = enemigo.vida - daño
+        print(self.nombre, "ha realizado", daño, " puntos de daño a ", enemigo.nombre)
+        print("Vida de ", enemigo.nombre, " es", enemigo.vida)
 
-#Variable del constructor vacío de la clase
-mi_personaje = Personaje()
-#Modificando los valores de los atributos
-mi_personaje.nombre = "MrBeast"
-mi_personaje.fuerza = 9001
-print ("El nombre del personaje es ",mi_personaje.nombre)
-print ("La fuerza del personaje es ",mi_personaje.fuerza)
+#Variable del constructor de la clase
+mi_personaje = Personaje("Trakalosa de monterrey", 70, 90, 50, 100)
+mi_enemigo = Personaje("La arrolladora banda limón", 60, 90, 40, 100)
+#print (mi_personaje.esta_vivo())
+#print (mi_personaje.dañar(mi_enemigo))
+#print(mi_personaje.dañar(mi_enemigo))
+mi_personaje.atributos()
+mi_personaje.atacar(mi_enemigo)
+mi_enemigo.atributos()
